2197d52bcee374c125a695af7ea8184d8a88336a
devgis <devgis@163.com>
2022-05-17 13:41:08 +0800

    Public
    Public

---
+++ Source/MongoTest/MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MongoTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btGGroup = new System.Windows.Forms.Button();
+            this.btRoleTable = new System.Windows.Forms.Button();
+            this.btUserTable = new System.Windows.Forms.Button();
+            this.btRoleUser = new System.Windows.Forms.Button();
+            this.dtDeleteTime = new System.Windows.Forms.DateTimePicker();
+            this.btDeleteRow = new System.Windows.Forms.Button();
+            this.tbEditRole = new System.Windows.Forms.TextBox();
+            this.btReadRole = new System.Windows.Forms.Button();
+            this.btEditUser = new System.Windows.Forms.Button();
+            this.tbEditUserName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btGGroup
+            // 
+            this.btGGroup.Location = new System.Drawing.Point(37, 42);
+            this.btGGroup.Name = "btGGroup";
+            this.btGGroup.Size = new System.Drawing.Size(75, 23);
+            this.btGGroup.TabIndex = 0;
+            this.btGGroup.Text = "GGroup";
+            this.btGGroup.UseVisualStyleBackColor = true;
+            this.btGGroup.Click += new System.EventHandler(this.btGGroup_Click);
+            // 
+            // btRoleTable
+            // 
+            this.btRoleTable.Location = new System.Drawing.Point(165, 42);
+            this.btRoleTable.Name = "btRoleTable";
+            this.btRoleTable.Size = new System.Drawing.Size(75, 23);
+            this.btRoleTable.TabIndex = 3;
+            this.btRoleTable.Text = "RoleTable";
+            this.btRoleTable.UseVisualStyleBackColor = true;
+            this.btRoleTable.Click += new System.EventHandler(this.btRoleTable_Click);
+            // 
+            // btUserTable
+            // 
+            this.btUserTable.Location = new System.Drawing.Point(293, 42);
+            this.btUserTable.Name = "btUserTable";
+            this.btUserTable.Size = new System.Drawing.Size(75, 23);
+            this.btUserTable.TabIndex = 4;
+            this.btUserTable.Text = "UserTable";
+            this.btUserTable.UseVisualStyleBackColor = true;
+            this.btUserTable.Click += new System.EventHandler(this.btUserTable_Click);
+            // 
+            // btRoleUser
+            // 
+            this.btRoleUser.Location = new System.Drawing.Point(165, 95);
+            this.btRoleUser.Name = "btRoleUser";
+            this.btRoleUser.Size = new System.Drawing.Size(75, 23);
+            this.btRoleUser.TabIndex = 5;
+            this.btRoleUser.Text = "Role.User";
+            this.btRoleUser.UseVisualStyleBackColor = true;
+            this.btRoleUser.Click += new System.EventHandler(this.btRoleUser_Click);
+            // 
+            // dtDeleteTime
+            // 
+            this.dtDeleteTime.Location = new System.Drawing.Point(281, 95);
+            this.dtDeleteTime.Name = "dtDeleteTime";
+            this.dtDeleteTime.Size = new System.Drawing.Size(87, 21);
+            this.dtDeleteTime.TabIndex = 6;
+            // 
+            // btDeleteRow
+            // 
+            this.btDeleteRow.Location = new System.Drawing.Point(374, 94);
+            this.btDeleteRow.Name = "btDeleteRow";
+            this.btDeleteRow.Size = new System.Drawing.Size(75, 23);
+            this.btDeleteRow.TabIndex = 7;
+            this.btDeleteRow.Text = "删除Role";
+            this.btDeleteRow.UseVisualStyleBackColor = true;
+            this.btDeleteRow.Click += new System.EventHandler(this.btDeleteRow_Click);
+            // 
+            // tbEditRole
+            // 
+            this.tbEditRole.Location = new System.Drawing.Point(37, 151);
+            this.tbEditRole.Name = "tbEditRole";
+            this.tbEditRole.Size = new System.Drawing.Size(100, 21);
+            this.tbEditRole.TabIndex = 8;
+            // 
+            // btReadRole
+            // 
+            this.btReadRole.Location = new System.Drawing.Point(158, 149);
+            this.btReadRole.Name = "btReadRole";
+            this.btReadRole.Size = new System.Drawing.Size(75, 23);
+            this.btReadRole.TabIndex = 9;
+            this.btReadRole.Text = "读取角色";
+            this.btReadRole.UseVisualStyleBackColor = true;
+            this.btReadRole.Click += new System.EventHandler(this.btReadRole_Click);
+            // 
+            // btEditUser
+            // 
+            this.btEditUser.Location = new System.Drawing.Point(374, 147);
+            this.btEditUser.Name = "btEditUser";
+            this.btEditUser.Size = new System.Drawing.Size(75, 23);
+            this.btEditUser.TabIndex = 10;
+            this.btEditUser.Text = "保存";
+            this.btEditUser.UseVisualStyleBackColor = true;
+            this.btEditUser.Click += new System.EventHandler(this.btEditUser_Click);
+            // 
+            // tbEditUserName
+            // 
+            this.tbEditUserName.Location = new System.Drawing.Point(251, 149);
+            this.tbEditUserName.Name = "tbEditUserName";
+            this.tbEditUserName.Size = new System.Drawing.Size(100, 21);
+            this.tbEditUserName.TabIndex = 11;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 231);
+            this.Controls.Add(this.tbEditUserName);
+            this.Controls.Add(this.btEditUser);
+            this.Controls.Add(this.btReadRole);
+            this.Controls.Add(this.tbEditRole);
+            this.Controls.Add(this.btDeleteRow);
+            this.Controls.Add(this.dtDeleteTime);
+            this.Controls.Add(this.btRoleUser);
+            this.Controls.Add(this.btUserTable);
+            this.Controls.Add(this.btRoleTable);
+            this.Controls.Add(this.btGGroup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mongodb查重工具";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btGGroup;
+        private System.Windows.Forms.Button btRoleTable;
+        private System.Windows.Forms.Button btUserTable;
+        private System.Windows.Forms.Button btRoleUser;
+        private System.Windows.Forms.DateTimePicker dtDeleteTime;
+        private System.Windows.Forms.Button btDeleteRow;
+        private System.Windows.Forms.TextBox tbEditRole;
+        private System.Windows.Forms.Button btReadRole;
+        private System.Windows.Forms.Button btEditUser;
+        private System.Windows.Forms.TextBox tbEditUserName;
+    }
+}
---
+++ Source/MongoTest/MainForm.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using MongoDB;
+using System.Collections;
+
+namespace MongoTest
+{
+    public partial class MainForm : Form
+    {
+        private Mongo mongo;
+        private IMongoDatabase GameData;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private void btGGroup_Click(object sender, EventArgs e)
+        {
+            var ggrouptable = GameData.GetCollection<Document>("GGroup");
+            var ggroups = ggrouptable.FindAll();
+
+            foreach (Document d in ggroups.Documents)
+            {
+                //key
+                var selector = new Document { { "key", d["key"] } };
+                var ggroupsamed = ggrouptable.Find(selector);
+                if (ggroupsamed.Documents.Count<Document>() > 1)
+                {
+                    int i = 1;
+                    foreach (Document redit in ggroupsamed.Documents)
+                    {
+                        var editselector = new Document { { "_id", redit["_id"] } };
+                        redit["key"] = d["key"].ToString() +"("+ i+")";
+                        ggrouptable.Update(redit, selector);
+                        i++;
+                        //更新
+                    }
+                }
+
+                //name
+                var selectornamed = new Document { { "name", d["name"] } };
+                var ggroupsamedname = ggrouptable.Find(selectornamed);
+                if (ggroupsamedname.Documents.Count<Document>() > 1)
+                {
+                    int i = 1;
+                    foreach (Document redit in ggroupsamedname.Documents)
+                    {
+                        var editselector = new Document { { "_id", redit["_id"] } };
+                        redit["name"] = d["name"].ToString() + "(" + i + ")";
+                        ggrouptable.Update(redit, editselector);
+                        i++;
+                        //更新
+                    }
+                }
+
+                //admin
+                var selectoradmin = new Document { { "admin", d["admin"] } };
+                var ggroupsamedadmin = ggrouptable.Find(selectoradmin);
+                if (ggroupsamedadmin.Documents.Count<Document>() > 1)
+                {
+                    int i = 1;
+                    foreach (Document redit in ggroupsamedadmin.Documents)
+                    {
+                        var editselector = new Document { { "_id", redit["_id"] } };
+                        redit["name"] = d["name"].ToString() + "(" + i + ")";
+                        ggrouptable.Update(redit, editselector);
+                        i++;
+                        //更新
+                    }
+                }
+                
+            }
+            MessageBox.Show("执行成功");
+        }
+
+
+        private void btRoleTable_Click(object sender, EventArgs e)
+        {
+            var roletable = GameData.GetCollection<Document>("RoleTable");
+            var roles = roletable.FindAll();
+
+            foreach (Document d in roles.Documents)
+            {
+                var selector = new Document { { "name", d["name"] } };
+                var rolesamed = roletable.Find(selector);
+                if (rolesamed.Documents.Count<Document>() > 1)
+                {
+                    int i = 1;
+                    foreach (Document redit in rolesamed.Documents)
+                    {
+                        var editselector = new Document { { "_id", redit["_id"] } };
+                        redit["name"] = d["name"].ToString() +"(" + i + ")";
+                        roletable.Update(redit, editselector);
+                        i++;
+                        //更新
+                    }
+                }
+            }
+            MessageBox.Show("执行成功");
+        }
+
+        private void btUserTable_Click(object sender, EventArgs e)
+        {
+            var usertable = GameData.GetCollection<Document>("UserTable");
+            var users = usertable.FindAll();
+
+            foreach (Document d in users.Documents)
+            {
+                var selector = new Document { { "user", d["user"] } };
+                var usersamed = usertable.Find(selector);
+                if (usersamed.Documents.Count<Document>() > 1)
+                {
+                    int i = 1;
+                    foreach (Document dedit in usersamed.Documents)
+                    {
+                        var editselector = new Document { { "_id", dedit["_id"] } };
+                        dedit["user"] = d["user"].ToString() + "(" + i + ")";
+                        usertable.Update(dedit, editselector);
+                        i++;
+                        //更新
+                    }
+                }
+            }          
+            MessageBox.Show("执行成功");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+        /// <summary>
+        ///   Setup the collection and insert some data into it.
+        /// </summary>
+        public void Setup()
+        {
+            var connstr = ConfigurationManager.AppSettings["GameData"];
+            if(String.IsNullOrEmpty(connstr))
+                throw new ArgumentNullException("Connection string not found.");
+            mongo = new Mongo(connstr);
+            mongo.Connect();
+            GameData = mongo["GameData"];
+        }
+
+        private void btRoleUser_Click(object sender, EventArgs e)
+        {
+            var roletable = GameData.GetCollection<Document>("RoleTable");
+            var usertable = GameData.GetCollection<Document>("UserTable");
+            var userlist = usertable.FindAll();
+            var roles = roletable.FindAll();
+
+            Dictionary<string, int> dicCount = new Dictionary<string, int>();
+
+            foreach (Document d in roles.Documents)
+            {
+                var selector = new Document { { "user", d["user"] } };
+
+                //var entity = collection.FindOne(query);
+                int icount = 0;
+                foreach (Document duser in userlist.Documents)
+                {
+                    //if (duser["user"].ToString().StartsWith(d["user"].ToString()))
+                    if (duser["user"].ToString().Split('(')[0].Equals(d["user"].ToString()))
+                    {
+                        icount++;
+                    }
+                }
+                //计算数量
+                if (icount <= 1)
+                {
+                    continue; //不存在直接退出 当前用户在用户表中不存在 或者只有一项 不进行处理
+                }
+                else
+                {
+                    //当前用户类似的名称在数据库中有多项
+                    var rolesamed = roletable.Find(selector);
+                    if (rolesamed.Documents.Count<Document>() > 0)
+                    {
+                        //查找user的数量
+                        int i = 1;
+                        foreach (Document redit in rolesamed.Documents)
+                        {
+                            //判断user不存在
+                            var userlist2 = usertable.Find(new Document { { "user", redit["user"] } });
+                            if (userlist2.Documents.Count<Document>() >= 0)
+                            {
+                                var editselector = new Document { { "_id", redit["_id"] } };
+                                redit["user"] = d["user"].ToString() + "(" + i + ")";
+                                roletable.Update(redit, editselector);
+                                i++;
+                                if (i > icount)
+                                {
+                                    i = 1;
+                                }
+                                //更新
+                            }
+                            //已经存在 不需要更新
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("执行成功");
+        }
+
+        private void btDeleteRow_Click(object sender, EventArgs e)
+        {
+            var roletable = GameData.GetCollection<Document>("RoleTable");
+            var roles = roletable.FindAll();
+
+            foreach (Document d in roles.Documents)
+            {
+                try
+                {
+                    DateTime dt = Convert.ToDateTime(d["uptimer"]);
+                    if (dt < dtDeleteTime.Value.Date)
+                    {
+                        roletable.Remove(d);
+                    }
+                }
+                catch
+                { }
+            }
+            MessageBox.Show("执行成功");
+        }
+        Document docEditRole = null;
+        private void btReadRole_Click(object sender, EventArgs e)
+        {
+            docEditRole = null;
+            if (string.IsNullOrEmpty(tbEditRole.Text.Trim()))
+            {
+                MessageBox.Show("请输入角色名！");
+                tbEditRole.Focus();
+            }
+            else
+            {
+                //查询
+                var roletable = GameData.GetCollection<Document>("RoleTable");
+                var selector = new Document { { "name", tbEditRole.Text } };
+                var rolesamed = roletable.Find(selector);
+                if (rolesamed.Documents.Count<Document>() >= 1)
+                {
+                    docEditRole = rolesamed.Documents.First<Document>();
+                    tbEditUserName.Text = docEditRole["user"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show(string.Format("没有名称为{0}的角色，请检查输入！", tbEditRole.Text));
+                    tbEditUserName.Text = string.Empty;
+                    tbEditRole.Focus();
+                }
+            }
+        }
+
+        private void btEditUser_Click(object sender, EventArgs e)
+        {
+            if (docEditRole == null)
+            {
+                MessageBox.Show("请先查找角色！");
+                tbEditRole.Focus(); 
+                return;
+            }
+            if (string.IsNullOrEmpty(tbEditUserName.Text.Trim()))
+            {
+                MessageBox.Show("请输入用户名！");
+                tbEditUserName.Focus();
+                return;
+            }
+            var roletable = GameData.GetCollection<Document>("RoleTable");
+            var editselector = new Document { { "_id", docEditRole["_id"] } };
+            docEditRole["user"] = tbEditUserName.Text;
+            roletable.Update(docEditRole, editselector);
+            MessageBox.Show("保存成功！");
+        }
+    }
+}
